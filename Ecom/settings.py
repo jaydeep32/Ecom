5b5fb0c0c8804e5b76a8b12cecbf521f95612519
Jaydeep <jaydipgajera2020@gmail.com>
2021-09-19 14:22:31 +0530

    GG

--- Ecom/settings.py
+++ Ecom/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import django_on_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -128,7 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
@@ -161,3 +162,8 @@ SWAGGER_SETTINGS = {
         }
     }
 }
+
+
+
+
+django_on_heroku.settings(locals())
