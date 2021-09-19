523460e111bd4cd89caa20f4534a4ce605b45367
Jaydeep <80737599+jaydeep32@users.noreply.github.com>
2021-09-19 14:01:50 +0530

    Update settings.py

--- Ecom/settings.py
+++ Ecom/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-w22@s4^r7_sei(rq&mum30np-$fo@#%&f7kd_)1!c#369uqo@g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*',]
 
 
 # Application definition
@@ -128,6 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
